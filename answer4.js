6dc85709f7ca07201d6ae117716dcf946ed5f777
yashsaini7060 <yk18052000@gmail.com>
2023-07-17 20:35:40 +0530

    all answers

--- answer4.js
+++ answer4.js
@@ -1,17 +1,17 @@
 //Callback hell refers to a situation in asynchronous programming where multiple nested callbacks makes the code difficult to read, understand, and maintain. It occurs when there are many asynchronous operations that depend on the completion of each other, resulting in nested code structures which is hard to read.
 
 //To overcome callback hell and improve code readability and maintainability, several techniques can be employed:
-//1.Promises
-//2.Async/await
-//3.Library support
-//4.Using named function
-
+//Write comments
+//Split functions into smaller functions
+//Using Async.js
+//Using Promises
+//Using Async-Await
 
 getData(function(a){
-  getMoreData(a, function(b){
-      getMoreData(b, function(c){ 
-          getMoreData(c, function(d){ 
-              getMoreData(d, function(e){ 
+  getMoreData1(a, function(b){
+      getMoreData2(b, function(c){ 
+          getMoreData3(c, function(d){ 
+              getMoreData4(d, function(e){ 
                   console.log('callback hell');
               });
           });
